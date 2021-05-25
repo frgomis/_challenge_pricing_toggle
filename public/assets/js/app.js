54be5b3191b0834e34acb4b95d76c6c323934db9
Francoise Gomis <gomisfrancoise@yahoo.com>
2021-05-25 15:22:10 +0200

    initialisation

---
+++ public/assets/js/app.js
@@ -0,0 +1,18 @@
+"use strict"
+// LES VARIABLES 
+let btn = document.querySelector("button");
+let label = document.querySelector("label");
+let prices = document.querySelectorAll(".price");
+
+console.log(prices);
+label.addEventListener('click', )
+
+
+
+
+//  btn LEARN MORE
+btn.addEventListener('click', function(){
+    btn.classList.toggle('active')
+    
+})
+
