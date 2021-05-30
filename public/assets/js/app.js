7f23da2504070be37d4b5b96b89347e667014239
Francoise Gomis <gomisfrancoise@yahoo.com>
2021-05-30 17:42:24 +0200

    màj toggle

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,53 +2,20 @@
 // LES VARIABLES 
 let headerBtn = document.querySelector(".slider");
 let freq = document.querySelectorAll(".price");
-// let perMonth = document.querySelectorAll(".month");
-// let perYear = document.querySelectorAll(".year");
 let btnLearnMore = document.querySelectorAll("button");
-console.log(freq);
+console.log(btnLearnMore);
 //--------------------------------------------------
 // PRICE : Month vs Year
-// headerBtn.addEventListener('click', ()=>{
-//     perYear.classList.toggle('active');
-//     perMonth.classList.toggle('active');
-// });
-
-freq.forEach( (freqs) =>{
-    headerBtn.addEventListener('click', () => {
-        console.log(headerBtn);
-        for(let j = 0; j < freqs.length; j++) {
-            freq[j].classList.toggle('active');
-            
-        }
-    });
+headerBtn.addEventListener('click', ()=>{
+    for(let i=0; i<freq.length; i++){
+        freq[i].classList.toggle('active');
+     }
 });
 //--------------------------------------------------
 //  BUTTON - LEARN MORE
-btnLearnMore.forEach((elements) =>{
-    
-    elements.addEventListener('click', () => {
-        for(let i = 0; i < btnLearnMore.length; i++) {
-            btnLearnMore[i].classList.toggle('active');
-            
-        }
-        
-    });
-
+btnLearnMore.addEventListener('click', () => {
+    for(let i = 0; i < btnLearnMore.length; i++) {
+        btnLearnMore[i].classList.toggle('active');
+    }
 });
 
-
-// let footers = document.querySelectorAll('footer');
-
-// let iconsShare = document.querySelectorAll('.icons');
-
-
-// iconsShare.forEach( (elementsdutableau) => {
-
-//     elementsdutableau.addEventListener('click', ()=> {
-
-//         for(i=0; i < footers.length; i++) {
-//             footers[i].classList.toggle('inactive');
-//             footers[i].classList.toggle('active');
-//         }
-//     });
-// });
